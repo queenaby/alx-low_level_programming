3ff7801eb22979dd49619b0d3deca2daa5c6ee98
queenaby <ibk.bisola@gmail.com>
2023-07-13 11:20:31 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- *main - Entry point
- *
- *Return: Always 0 (Success)
+ * main - main entry point
+ * return : always 0 (success)
  */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
