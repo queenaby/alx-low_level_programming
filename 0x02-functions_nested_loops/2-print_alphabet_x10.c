46b272806673c49715f608193f84e3a10a5b0ee9
queenaby <ibk.bisola@gmail.com>
2023-07-19 09:38:37 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function that prints alphabet in lowercase x10
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int j;
+	char i;
+
+	for (j = 0 ; j < 10 ; j++)
+	{
+		for (i = 'a' ; i <= 'z' ; i++)
+			_putchar(i);
+	}
+	_putchar('\n');
+}
