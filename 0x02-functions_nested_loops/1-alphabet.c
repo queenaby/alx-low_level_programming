7f24217fb5364aa09acf338cc33f1b5de3087dbc
queenaby <ibk.bisola@gmail.com>
2023-07-19 09:05:56 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - program to print lowercase alphabets
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char j;
+
+	for (j = 'a' ; j <= 'z' ; j++)
+		_putchar(j);
+	_putchar ('\n');
+}
