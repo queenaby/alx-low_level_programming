34e68dd7089623f994f4b0efafd48e435dbbe041
queenaby <ibk.bisola@gmail.com>
2023-08-02 23:40:16 -0700

    task8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * set_string - Entry point
+ * @s: parameter 1
+ * @to: parameter 2
+ * Return: Always 0 (Success)
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
