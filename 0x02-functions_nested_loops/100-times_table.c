b642fda07c006ab06a5dc736afda35e674361b61
queenaby <ibk.bisola@gmail.com>
2023-08-06 04:20:48 -0700

    update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
@@ -45,5 +45,4 @@ void print_times_table(int n)
 		}
 	}
 }
-
 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,7 @@
-int _putchar(char c);
+#ifndef main_h
+#define main_h
+
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -9,5 +12,9 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to_98(int n)i;
+void print_to_98(int n);
 void print_times_table(int n);
+
+#endif
+
+
